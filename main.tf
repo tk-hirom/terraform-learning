61dfebc866705252f7bd81d7fe8fd693c4e50018
tk-hirom <git config --global user.email exertmyoverspac@icloud.com>
2024-05-12 23:31:39 +0900

    インスタンスへの変更

--- main.tf
+++ main.tf
@@ -4,6 +4,16 @@ provider "aws" { // AWSにリソース作るという宣言
 }
 
 resource "aws_instance" "hello-world" { // EC2インスタンス作成
-  ami = "ami-0ab3794db9457b60a" // Amazon Machine Image　仮想サーバーイメージ インスタンスのテンプレート
+  ami = "ami-0d0150aa305b7226d" // Amazon Machine Image　仮想サーバーイメージ インスタンスのテンプレート
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+  // インスタンス起動時に実行されるスクリプト(user_data)
+  user_data = <<EOF
+#!/bin/bash
+amazon-linux-extras install -y nginx1.12
+systemctl start nginx
+EOF  
 }
