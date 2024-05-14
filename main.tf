9e7c8454e0ec41b1d10c01b6f9acfdb5e3482165
tk-hirom <git config --global user.email exertmyoverspac@icloud.com>
2024-05-15 00:12:34 +0900

    create framework

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
-provider "aws" { // AWSにリソース作るという宣言
-  profile = "terraform" //認証情報管理に使用されるもの
-  region = "ap-northeast-1"
+terraform {
+  required_version = ">=1.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.49.0"
+    }
+  }
 }
 
-resource "aws_instance" "hello-world" { // EC2インスタンス作成
-  ami = "ami-0d0150aa305b7226d" // Amazon Machine Image　仮想サーバーイメージ インスタンスのテンプレート
-  instance_type = "t2.micro"
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
 
-  tags = {
-    Name = "HelloWorld"
-  }
-  // インスタンス起動時に実行されるスクリプト(user_data)
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF  
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
