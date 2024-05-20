8520f38daa495596666b020c2dce2eaa1b12d7fa
tk-hirom <exertmyoverspac@icloud.com>
2024-05-20 23:24:45 +0900

    パラメータグループの追加

---
+++ rds.tf
@@ -0,0 +1,18 @@
+# ---------------------------------------------
+# RDS parameter group
+# データベースをカスタマイズする設定集
+# ---------------------------------------------
+resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
+  name   = "${var.project}-${var.environment}-mysql-standalone-parametergroup"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
