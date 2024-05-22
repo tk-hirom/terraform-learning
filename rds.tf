ef38ea6fba82c3da8f232bb9bab2845aef70c13a
tk-hirom <exertmyoverspac@icloud.com>
2024-05-22 20:58:18 +0900

    RDS作成

--- rds.tf
+++ rds.tf
@@ -49,3 +49,43 @@ resource "random_string" "db_password" {
   length  = 16
   special = false
 }
+
+resource "aws_db_instance" "mysql_standalone" {
+  identifier_prefix      = "${var.project}-${var.environment}-mysql-standalone"
+  allocated_storage      = 20
+  max_allocated_storage  = 50
+  storage_type           = "gp2"
+  storage_encrypted      = false
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  engine         = "mysql"
+  engine_version = "8.0.35"
+  username       = "admin"
+  password       = random_string.db_password.result
+
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  instance_class = "db.t3.micro"
+
+  backup_window              = "04:00-05:00" // バックアップをメンテの先にするようにしている
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = true
+  skip_final_snapshot = false
+
+  apply_immediately = true
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-mysql-standalone"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
